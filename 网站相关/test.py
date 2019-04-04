61ab4625812e65b85613a8e182ffeb1d739fdd9f
unknown <478830798@qq.com>
2019-04-04 08:55:38 +0800

    scheme

---
+++ 网站相关/test.py
@@ -0,0 +1,14 @@
+# arr = [[1,2,3],[4,5,6],[7,8,9]]
+# length = len(arr[0])
+# data=[]
+# for i in range(length):
+#     data.append([row[i] for row in arr])
+# print(data)
+# arr='篮板'
+# try:
+#     arr1 = int(arr)
+# except:
+#     arr1 = 33
+# print(arr1)
+str1 = '开赛：2019年04月03日 08:00'
+print(str1[3:7] + "-"+str1[8:10]+'-' + str1[11:13])
