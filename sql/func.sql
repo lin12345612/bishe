1faa16d158a9e47eba397c07f43a639cb7c4b406
unknown <478830798@qq.com>
2019-09-06 10:47:34 +0800

    add_sql

---
+++ sql/func.sql
@@ -0,0 +1,31 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50527
+ Source Host           : localhost:3306
+ Source Schema         : myweb
+
+ Target Server Type    : MySQL
+ Target Server Version : 50527
+ File Encoding         : 65001
+
+ Date: 06/09/2019 10:44:23
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for func
+-- ----------------------------
+DROP TABLE IF EXISTS `func`;
+CREATE TABLE `func`  (
+  `fid` int(255) NOT NULL AUTO_INCREMENT,
+  `djgn` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '点击功能',
+  `djsj` datetime NULL DEFAULT NULL COMMENT '点击时间',
+  PRIMARY KEY (`fid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ sql/visit.sql
@@ -0,0 +1,33 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50527
+ Source Host           : localhost:3306
+ Source Schema         : myweb
+
+ Target Server Type    : MySQL
+ Target Server Version : 50527
+ File Encoding         : 65001
+
+ Date: 06/09/2019 10:44:13
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for visit
+-- ----------------------------
+DROP TABLE IF EXISTS `visit`;
+CREATE TABLE `visit`  (
+  `vid` int(255) NOT NULL AUTO_INCREMENT,
+  `fwip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '访问ip',
+  `fwsj` datetime NULL DEFAULT NULL COMMENT '访问时间',
+  `fwcs` int(255) NULL DEFAULT NULL COMMENT '访问此时',
+  `fwzd` int(255) NULL DEFAULT NULL COMMENT '访问终端 0、pc端 1、移动端',
+  PRIMARY KEY (`vid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
